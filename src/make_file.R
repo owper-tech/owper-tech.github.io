328a80ff3be38fd1db0f498e6854818f7d10e560
Paul Julian <SwampThingPaul@users.noreply.github.com>
2020-12-05 14:12:40 -0500

    SA reports posted

---
+++ src/make_file.R
@@ -0,0 +1,18 @@
+## 
+## Report Rendering
+##
+## 
+## Code was compiled by Paul Julian
+## contact info: paul.julian@floridadep.gov
+
+
+## Appendix A report
+report.file="C:/Julian_LaCie/_GitHub/owper-SettlementAgreement/SRSUpdate.Rmd"
+out.loc="C:/Julian_LaCie/_GitHub/owper-tech.github.io/reports/SRSUpdate.html"
+rmarkdown::render(report.file,output_file = out.loc)
+
+
+## Appendix B report
+report.file="C:/Julian_LaCie/_GitHub/owper-SettlementAgreement/LNWRUpdate.Rmd"
+out.loc="C:/Julian_LaCie/_GitHub/owper-tech.github.io/reports/LNWRUpdate.html"
+rmarkdown::render(report.file,output_file = out.loc)
